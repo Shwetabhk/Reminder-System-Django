714e89ce95758932fd95e5eebc5ab308427b1a5b
ShwetabhK <shwetabh002@gmail.com>
2018-02-10 09:24:13 +0530

    Final Reminder-System Documented1

--- Reminder_System/settings.py
+++ Reminder_System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^$g30zoci5qzi7k0fqvzuqpyctfq5$67tw4^a$4cl^f#26++(7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shwetabh.pythonanywhere.com']
 
 
 # Application definition
